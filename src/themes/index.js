2c5743f219aa4041f96651b2349cec7df274eb3b
daler <developer.daler@gmail.com>
2021-05-30 07:01:21 +0500

    added one new theme

--- src/themes/index.js
+++ src/themes/index.js
@@ -1,11 +1,13 @@
 import defaultTheme from './defaultTheme'
 import violetTheme from './violetTheme'
 import greyTheme from './greyTheme'
+import lightTheme from './lightTheme'
 
 const Themes = {
   defaultTheme,
   violetTheme,
-  greyTheme
+  greyTheme,
+  lightTheme
 }
 
 export default Themes
---
+++ src/themes/lightTheme.jsx
@@ -0,0 +1,33 @@
+const index = {
+  name: 'defaultTheme',
+  type: 'light',
+  common: {
+    white: '#fff',
+    black: '#000'
+  },
+  primary: {
+    light: '#F0F0F0',
+    main: '#D5D5D5',
+    dark: '#ACACAC',
+    contrastText: '#212121'
+  },
+  secondary: {
+    main: '#fff',
+    dark: '#F0F0F0',
+    contrastText: '#000'
+  },
+  text: {
+    primary: '#fff',
+    secondary: '#212121',
+    disabled: '#938E97'
+  },
+  background: {
+    paper: '#fff',
+    default: 'F0F0F0'
+  },
+  scroll: {
+    thumbColor: '#D5D5D5' 
+  }
+}
+
+export default index
